973bfa03857d39e2d218fd415a57c65dd1a4f3cc
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-04-05 12:54:11 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+function submitData(name,email){
+   return fetch(`http://localhost:3000/users`,{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify({name,email})
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+
+    .then(data => {
+        const id = data.id;
+        document.body.append(id);
+    })
+    .catch(error => {
+        document.body.append(error.message);
+    });
+} 
